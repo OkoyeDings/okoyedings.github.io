9239851424a37c620a4117991de38cece09a2cfe
Okoye Dings <100094017+OkoyeDings@users.noreply.github.com>
2024-06-25 00:54:55 +0100

    update

--- crypto/assets/js/ap.js
+++ crypto/assets/js/ap.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 orders.forEach(order =>{
     const tr = document.createElement('tr');
     const trContent = `
@@ -11,4 +12,35 @@ orders.forEach(order =>{
     document.querySelector('table tbody').appendChild(tr);
 });
 
+=======
+<<<<<<< HEAD
+orders.forEach(order =>{
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.status === 'declined' ? 'danger' : order.status === 'pending' ? 'warning' : 'primary'}">${order.status}</td>
+        <td class="primary"> details</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+});
+
+=======
+orders.forEach(order =>{
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.status === 'declined' ? 'danger' : order.status === 'pending' ? 'warning' : 'primary'}">${order.status}</td>
+        <td class="primary"> details</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+});
+
+>>>>>>> aa85effbb3cc709e4f63e4ee70e82891af6a2fdb
+>>>>>>> Stashed changes
 //59:50
--- crypto/assets/js/order.js
+++ crypto/assets/js/order.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const orders = [
     {
         productName: 'javaScript tutorial',
@@ -17,4 +18,47 @@ const orders = [
         paymentStatus: 'paid',
         status: 'active'
     },
+=======
+<<<<<<< HEAD
+const orders = [
+    {
+        productName: 'javaScript tutorial',
+        productNumber: '$85,743',
+        paymentStatus: 'due',
+        status: 'pending'
+    },
+    {
+        productName: 'CSS full course',
+        productNumber: '$97,245',
+        paymentStatus: 'refunded',
+        status: 'declined'
+    },
+    {
+        productName: 'flexbox tutorial',
+        productNumber: '$36,452',
+        paymentStatus: 'paid',
+        status: 'active'
+    },
+=======
+const orders = [
+    {
+        productName: 'javaScript tutorial',
+        productNumber: '$85,743',
+        paymentStatus: 'due',
+        status: 'pending'
+    },
+    {
+        productName: 'CSS full course',
+        productNumber: '$97,245',
+        paymentStatus: 'refunded',
+        status: 'declined'
+    },
+    {
+        productName: 'flexbox tutorial',
+        productNumber: '$36,452',
+        paymentStatus: 'paid',
+        status: 'active'
+    },
+>>>>>>> aa85effbb3cc709e4f63e4ee70e82891af6a2fdb
+>>>>>>> Stashed changes
 ]
